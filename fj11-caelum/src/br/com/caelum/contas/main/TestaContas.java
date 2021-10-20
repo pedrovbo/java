a47876cc831ab3d07071bd0e33d758d912fc3fd7
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-19 23:04:05 -0300

    add deposita method

--- fj11-caelum/src/br/com/caelum/contas/main/TestaContas.java
+++ fj11-caelum/src/br/com/caelum/contas/main/TestaContas.java
@@ -1,12 +1,23 @@
 package br.com.caelum.contas.main;
 
 import br.com.caelum.javafx.api.main.TelaDeContas;
+import br.com.caelum.javafx.api.util.Evento;
 
 public class TestaContas {
 
 	public static void main(String[] args) {
 		TelaDeContas.main(args);
 		
+	}
+	
+	public void deposita(Evento evento) {
+		double valorDigitado = evento.getDouble("valor");
+		this.conta.deposita(valorDigitado);
+	}
+	
+	public void saca(Evento evento) {
+		double valorDigitado = evento.getDouble("valor");
+		this.conta.deposita(valorDigitado);
 	}
 	
 	
