db43ea4602cb91889892dee3b642a17e7299d503
pedrovbo <pedrovicboliveira@gmail.com>
2021-11-16 22:17:30 -0300

    add more java exercises

---
+++ basic-studies/src/com/exercises/loops/Exercise_10.java
@@ -0,0 +1,22 @@
+package com.exercises.loops;
+
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+public class Exercise_10 {
+    public static void main(String[] args) {
+        int[] numeros = new int[20];
+        Random aleatorio = new Random();
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = aleatorio.nextInt(100);
+            System.out.println("Valor aleatório = " + numeros[i] + " incluído no índice = " + i);
+        }
+
+
+
+    }
+}
